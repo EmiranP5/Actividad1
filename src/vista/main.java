713992de6cf0960fd2a5011986885ca3f07b19f1
eemusic <mirandaperezesdrasesteban@gmail.com>
2024-07-30 22:14:52 -0600

    actividad 1 Formularios

---
+++ src/vista/main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author JDani
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*Quita el comentario para mostrar el otro formulario*/
+        
+        /////// FORMULARIO CLIENTE /////////
+        
+        frm_cliente frm1 = new frm_cliente();
+        frm1.show();
+        
+        ////// FORMULARIO EMPLEADO /////////
+        
+        frm_empleado frm2 = new frm_empleado();
+        frm2.show();
+        
+        /////////////////////////////////////////
+    }
+    
+}
